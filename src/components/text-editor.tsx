4b75a12d2b3a28febb9406dc64b2ec22a41e3732
Andre <g.andrerichards@gmail.com>
2022-01-06 21:25:23 -0500

    addl styling

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -5,6 +5,7 @@ import "./text-editor.css";
 const TextEditor: React.FC = () => {
   const ref = useRef<HTMLDivElement | null>(null);
   const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState("# Editor");
 
   useEffect(() => {
     const listener = (e: MouseEvent) => {
@@ -24,14 +25,16 @@ const TextEditor: React.FC = () => {
   if (editing) {
     return (
       <div className="text-editor" ref={ref}>
-        <MDEditor />
+        <MDEditor value={value} onChange={(val) => setValue(val || "")} />
       </div>
     );
   }
 
   return (
-    <div onClick={() => setEditing(true)}>
-      <MDEditor.Markdown source={"# Header"} />
+    <div className="text-editor card" onClick={() => setEditing(true)}>
+      <div className="card-content">
+        <MDEditor.Markdown source={value} />
+      </div>
     </div>
   );
 };
