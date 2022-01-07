9348250b84770f11ee062c912e5dd855497f5e1c
Andre <g.andrerichards@gmail.com>
2022-01-06 20:53:17 -0500

    add text editor and ext/setup click toggle

---
+++ src/components/text-editor.tsx
@@ -0,0 +1,38 @@
+import { useState, useEffect, useRef } from "react";
+import MDEditor from "@uiw/react-md-editor";
+
+const TextEditor: React.FC = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [editing, setEditing] = useState(false);
+
+  useEffect(() => {
+    const listener = (e: MouseEvent) => {
+      if (ref.current && e.target && ref.current.contains(e.target as Node)) {
+        console.log("elem clicked in editor");
+        return;
+      }
+      console.log("elem clicked NOT in editor");
+      setEditing(false);
+    };
+    document.addEventListener("click", listener, { capture: true });
+
+    return () =>
+      document.removeEventListener("click", listener, { capture: true });
+  }, []);
+
+  if (editing) {
+    return (
+      <div ref={ref}>
+        <MDEditor />
+      </div>
+    );
+  }
+
+  return (
+    <div onClick={() => setEditing(true)}>
+      <MDEditor.Markdown source={"# Header"} />
+    </div>
+  );
+};
+
+export default TextEditor;
