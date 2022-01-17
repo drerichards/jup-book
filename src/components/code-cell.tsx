b7287a22df1ae6bd4734f092a08b943b027bcc75
Andre <g.andrerichards@gmail.com>
2022-01-17 03:15:25 -0500

    add func to accumulate code for var global scope across code cells

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -14,27 +14,44 @@ interface CodeCellProps {
 const CodeCell: FC<CodeCellProps> = ({ cell }) => {
   const { updateCell, createBundle } = useActions();
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
+  const cumulativeCode = useTypedSelector((state) => {
+    if (state.cells) {
+      const { data, order } = state.cells;
+      const orderedCells = order.map((id: string | number) => data[id]);
+
+      const cumulativeCode = [];
+      for (let c of orderedCells) {
+        if (c.type === "code") cumulativeCode.push(c.content);
+        if (c.id === cell.id) break; // stops when it gets to cell id thats passed via props
+      }
+      return cumulativeCode;
+    }
+  });
+
+  console.log(cumulativeCode);
 
   useEffect(() => {
-    const runBundler = (id: string, content: string) => {
-      createBundle(id, content);
+    const runBundler = (id: string, content: string | undefined): void => {
+      if (content) {
+        createBundle(id, content);
+      }
     };
 
     if (!bundle) {
       // exec bundle creation on first load
-      runBundler(cell.id, cell.content);
+      runBundler(cell.id, cumulativeCode?.join("\n"));
       return;
     }
 
     const timer = setTimeout(async () => {
       // bundles the typed code after 1.2 sec at a time
-      runBundler(cell.id, cell.content);
+      runBundler(cell.id, cumulativeCode?.join("\n"));
     }, 1200);
 
     return () => clearTimeout(timer);
     // bundle dep would create inf loop
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cell.id, cell.content, createBundle]);
+  }, [cell.id, createBundle]);
 
   return (
     <ResizableFrame direction="vertical">
